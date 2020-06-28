5e2c3416182cd6521b1ab91c7e7c4609d4450bca
Daniel Cochran <dan.s.cochran@gmail.com>
2020-06-28 11:27:20 -0400

    Added initialzr source

---
+++ src/main/java/com/mtb/ese/bootsvc/BootsvcApplication.java
@@ -0,0 +1,13 @@
+package com.mtb.ese.bootsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtb/ese/bootsvc/BootsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtb.ese.bootsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
